e81b70938ddc905ffa17a3cfa6727bef081d5636
Miguel Marquina <mmarquina@ue003cofopri.gob.pe>
2026-01-15 17:59:13 -0500

    Avance del hu para consultar distritos

--- src/app/services/distritos.service.ts
+++ src/app/services/distritos.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import {environment} from '../../environments/environment';
 import { ApiResponse } from '../models/api-response.model';
 import {UsuarioOrganizacionesData} from '../models/usuario-organizaciones.model';
+import {UsuarioOrganizacionesResponse} from '../interfaces/UsuarioOrganizacionesResponse';
 
 
 @Injectable({
@@ -13,15 +14,14 @@ export class DistritosService {
 
   constructor(private http: HttpClient) { }
 
-  buscar(nombre: string, page: number = 0, size: number = 6): Observable<ApiResponse<UsuarioOrganizacionesData>> {
+  buscar(nombre: string, page: number = 0, size: number = 6): Observable<UsuarioOrganizacionesResponse> {
     const params = new HttpParams()
       .set('nombre', nombre ?? '')
       .set('page', page)
       .set('size', size);
 
-    // OJO: aquí debe ser la URL COMPLETA del endpoint
-    // Ej: `${environment.urlWebApiSecurity}/consultaDatosUsuarioPorNombre`
-    return this.http.get<ApiResponse<UsuarioOrganizacionesData>>(
+
+    return this.http.get<UsuarioOrganizacionesResponse>(
       `${environment.urlWebApiSecurity}consultaDatosUsuarioPorNombre`,
       { params }
     );
