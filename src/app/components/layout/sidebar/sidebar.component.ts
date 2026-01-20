5087950c72346aee5b1beb7abbf65f54261ed0a3
Joel <joelssdesigner@gmail.com>
2026-01-20 18:04:45 -0500

    header-css

--- src/app/components/layout/sidebar/sidebar.component.ts
+++ src/app/components/layout/sidebar/sidebar.component.ts
@@ -1,9 +1,10 @@
 import { Component, EventEmitter, Output     } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
